c485caed26d2dfc369b2f7273c22a039b5d054ff
megan gosnell <megan_gosnell@aol.co.uk>
2021-02-09 14:08:51 +0000

    file created

---
+++ jasmine-standalone-3/src/fizzBuzz.js
@@ -0,0 +1,17 @@
+class Fizzbuzz {
+    _isDivisibleBy(divisor, number) {
+        return number % divisor === 0;
+   }
+   
+    play(number) {
+        if (this._isDivisibleBy(15, number)) {
+            return 'Fizzbuzz';
+        } else if (this._isDivisibleBy(5, number)) {
+            return 'Buzz'; 
+        }else if (this._isDivisibleBy(3, number)) {
+            return 'Fizz'; 
+        } else { 
+            return number;
+        }
+    }
+} 
